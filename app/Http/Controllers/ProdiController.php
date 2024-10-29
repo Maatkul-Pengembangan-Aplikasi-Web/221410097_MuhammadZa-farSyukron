4c3da6bece680a4b95b39c0f11a28345d0af4972
you <you@example.com>
2024-10-29 12:18:21 +0700

    fungsi tambah data add

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -9,6 +9,27 @@ class ProdiController extends Controller
 {
     public function index()
     {
+        $prodis = Prodi::all(); 
+        return view('prodi.index', compact('prodis'));
+
         return view('prodi.index');
+    }
+
+    public function create()
+    {
+        return view('prodi.create');
+    }
+
+    public function save(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required'
+        ]);
+
+        Prodi::create([
+            'nama' => $request->nama
+        ]);
+
+        return redirect()->route('prodi')->with('success', 'Program Studi berhasil ditambahkan');
     }
 }
